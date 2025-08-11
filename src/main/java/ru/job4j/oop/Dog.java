e8a91347be08e7280ab42fbb9e0c562a9d8046d4
bamvam <kyrartikal@gmail.com>
2025-08-11 21:00:29 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();  // Первый объект
+        Dog sharik = new Dog();   // Второй объект
+        Dog zhychka = new Dog();  // Третий объект
+    }
+}
