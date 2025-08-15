99e759c23708bb4535ab1ce35ec5b739ed05418a
bamvam <kyrartikal@gmail.com>
2025-08-15 20:43:23 +0300

    1.5. Поля объекта [#166083 #550308]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -20,13 +20,13 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("There is gav's food.");
         Cat gav = new Cat();
-        gav.giveNick("Gav");  // Устанавливаем кличку
+        gav.giveNick("Gav");
         gav.eat("cutlet");
         gav.show();
 
         System.out.println("There is black's food.");
         Cat black = new Cat();
-        black.giveNick("Black");  // Устанавливаем кличку
+        black.giveNick("Black");
         black.eat("fish");
         black.show();
     }
