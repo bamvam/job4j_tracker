3a70efb6a86fdf5b5663174176fc202218d2d69a
bamvam <kyrartikal@gmail.com>
2025-09-18 13:20:52 +0300

    1.6. Взаимодействие объектов [#166087 #550412]

--- src/main/java/ru/job4j/oop/Fox.java
+++ src/main/java/ru/job4j/oop/Fox.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 public class Fox {
+
     public void tryEat(Ball ball) {
         System.out.print("Лиса пытается съесть колобка. ");
         ball.tryRun(true);
--- src/main/java/ru/job4j/oop/Hare.java
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 public class Hare {
+
     public void tryEat(Ball ball) {
         System.out.print("Заяц пытается съесть колобка. ");
         ball.tryRun(false);
