b89954b7fa99197c08cf902e96cad2c080ad5ead
bamvam <kyrartikal@gmail.com>
2025-09-27 18:08:23 +0300

    8. Что такое валидация? [#246864 #552342]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -68,7 +68,6 @@ public class Tracker {
         }
 
         System.arraycopy(items, index + 1, items, index, size - index - 1);
-
         items[size - 1] = null;
         size--;
         return true;
