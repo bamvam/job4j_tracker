ed22b45e5679f3450f2d5099faa7b226dbb7341e
bamvam <kyrartikal@gmail.com>
2025-10-09 13:02:58 +0300

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -51,7 +51,6 @@ public class StartUI {
                     System.out.println("Ошибка удаления заявки.");
                 }
             } else if (select == 4) {
-                // Блок для поиска заявки по id
                 System.out.println("=== Вывод заявки по id ===");
                 System.out.print("Введите id: ");
                 int id = Integer.parseInt(scanner.nextLine());
